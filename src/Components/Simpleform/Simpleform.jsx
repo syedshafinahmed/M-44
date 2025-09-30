d46862c4184b71702647349fc0475185f3b0b3ec
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-30 11:16:12 +0600

    onSubmit & e.target

---
+++ src/Components/Simpleform/Simpleform.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Simpleform = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='name' placeholder='Your Name' /><br />
+                <input type="email" name="email" id="" placeholder='Your Email' /> <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default Simpleform;
