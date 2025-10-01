6c0cf7ee257f7e4f440d8201c40925e8498c8686
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-01 17:49:15 +0600

    controlled component

--- src/Components/ControlledField/ControlledField.jsx
+++ src/Components/ControlledField/ControlledField.jsx
@@ -3,12 +3,22 @@ import React, { useState } from 'react';
 const ControlledField = () => {
 
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log("submitted");
     }
     const handlePassOnChange = (e) => {
         console.log(e.target.value);
+        setPassword(e.target.value);
+
+        if(password.length < 6) {
+            setError('Password must be 6 character or longer');
+        }
+        else{
+            setError('');
+        }
+
     }
     return (
         <div>
@@ -17,6 +27,7 @@ const ControlledField = () => {
                 <input type="password" name="password" placeholder='Password' defaultValue={password} onChange={handlePassOnChange} required /><br />
                 <input type="submit" value="Submit" />
             </form>
+            <p className='text-red-600'><small>{error}</small></p>
         </div>
     );
 };
---
+++ src/Components/Uncontrolledfield/Uncontrolledfield.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Uncontrolledfield = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Uncontrolledfield;
