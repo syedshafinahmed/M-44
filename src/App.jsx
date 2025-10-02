42436647f96e2045afb953f83d49281c4b6755a8
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-02 17:41:42 +0600

    uncontrolled component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import ControlledField from './Components/ControlledField/ControlledField'
 import Formaction from './Components/Formaction/Formaction'
 import Simpleform from './Components/Simpleform/Simpleform'
+import Uncontrolledfield from './Components/Uncontrolledfield/Uncontrolledfield'
 
 function App() {
   
@@ -12,7 +13,8 @@ function App() {
       <h1>React Form</h1>
       {/* <Simpleform></Simpleform> */}
       {/* <Formaction></Formaction> */}
-      <ControlledField></ControlledField>
+      {/* <ControlledField></ControlledField> */}
+      <Uncontrolledfield></Uncontrolledfield>
       
     </>
   )
--- src/Components/Uncontrolledfield/Uncontrolledfield.jsx
+++ src/Components/Uncontrolledfield/Uncontrolledfield.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Uncontrolledfield = () => {
+    const passwordRef = useRef('');
+    const emailRef = useRef('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(emailRef.current.value);
+        console.log(passwordRef.current.value);
+    }
+
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <input ref={emailRef} type="email" name="" placeholder='email' /><br />
+                <input ref={passwordRef} type="password" name="" placeholder='password' /><br />
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
