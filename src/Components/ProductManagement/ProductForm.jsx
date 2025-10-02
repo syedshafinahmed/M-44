1938400d852f29717f2ee974e8f71b8e862cfbb0
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-02 20:34:19 +0600

    product management

--- src/Components/ProductManagement/ProductForm.jsx
+++ src/Components/ProductManagement/ProductForm.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProductForm = () => {
+const ProductForm = ({ handleAddProduct }) => {
     const handelProductSubmit = (e) => {
         e.preventDefault();
         const name = e.target.name.value;
         const price = e.target.price.value;
         const quantity = e.target.quantity.value;
 
+        const [vul, setError] = useState('');
+        // const [error, setError] = useState('');
+        if(name.length === 0){
+            setError('please provide a product name');
+        }
+        else if(price.length === 0){
+            setError('please provide a price');
+        }
+        else if(price < 0){
+            setError('price cannot be negative');
+        }
+        else{
+            setError('');
+        }
+
         const newProduct = {
             name,
             price,
             quantity
         };
-        console.log(newProduct);
+        // console.log(newProduct);
+        handleAddProduct(newProduct);
     }
     return (
         <div>
@@ -23,6 +39,7 @@ const ProductForm = () => {
                 <input type="text" name="quantity" placeholder='Product Quantity' id="" /><br />
                 <input type="submit" value="Submit" />
             </form>
+            <p className='text-red-700'><small>{vul}</small></p>
         </div>
     );
 };
